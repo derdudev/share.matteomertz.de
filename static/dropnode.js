b42a0d2257b8e25f2ec1b6d3a2e2390075819ce1
MDev <46203814+Matteo-Dev@users.noreply.github.com>
2023-05-04 20:42:08 +0200

    added styling of more and add subfolder button on folders

--- static/dropnode.js
+++ static/dropnode.js
@@ -73,7 +73,36 @@ class Dropnode {
             }
         }
 
-        folderHeader.append(expandBtn, folderName);
+        let moreButton = document.createElement("div");
+        moreButton.className = "expand-btn";
+        moreButton.innerHTML = "<svg xmlns=\"http://www.w3.org/2000/svg\" height=\"48\" viewBox=\"0 96 960 960\" width=\"48\"><path d=\"M184.783 635Q161 635 143.5 617.7T126 576.106Q126 551 143.373 534t41.769-17q23.458 0 41.658 16.894 18.2 16.894 18.2 41.5T226.783 617.5q-18.217 17.5-42 17.5Zm295.823 0Q456 635 438.5 617.7T421 576.106Q421 551 438.3 534t41.594-17Q505 517 522 533.894t17 41.5Q539 600 522.106 617.5t-41.5 17.5Zm294.288 0q-24.823 0-42.359-17.3Q715 600.4 715 576.106 715 551 732.677 534t42.5-17Q800 517 817.5 533.894t17.5 41.5Q835 600 817.358 617.5 799.717 635 774.894 635Z\"/></svg>";
+        tippy(moreButton, {
+            arrow: false,
+            content: "Umbenennen, Löschen, ...",
+            placement: "bottom",
+        })
+
+        let addSubFolder = document.createElement("div");
+        addSubFolder.className = "expand-btn";
+        addSubFolder.innerHTML = "<svg xmlns=\"http://www.w3.org/2000/svg\" height=\"48\" viewBox=\"0 96 960 960\" width=\"48\"><path d=\"M450 856V606H200v-60h250V296h60v250h250v60H510v250h-60Z\"/></svg>";
+        tippy(addSubFolder, {
+            arrow: false,
+            content: "Neuen Unterordner erstellen",
+            placement: "bottom",
+        })
+
+        folderHeader.append(expandBtn, folderName, moreButton, addSubFolder);
+
+        moreButton.style.display = "none";
+        addSubFolder.style.display = "none";
+        folderHeader.onmouseenter = () => {
+            moreButton.style.display = "block";
+            addSubFolder.style.display = "block";
+        }
+        folderHeader.onmouseleave = () => {
+            moreButton.style.display = "none";
+            addSubFolder.style.display = "none";
+        }
 
         dropdownElement.append(folderHeader, children);
 
@@ -85,7 +114,6 @@ class Dropnode {
             folderName: folderName
         }
 
-        this.domElement = dropdownElement;
         parent.append(dropdownElement);
     }
 
